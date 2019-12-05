d03be69273e4972c9b0c14165b0f665305ea6028
Jonathan Lee <jonalee99@gmail.com>
2019-12-04 22:19:30 -0500

    first draft

--- bots.py
+++ bots.py
@@ -126,7 +126,7 @@ class StudentBot:
 
         # If the node is terminal, return its value
         if asp.is_terminal_state(state):
-            return [asp.evaluate_state(state)[asp.get_start_state().player_to_move()], None]
+            return [100*asp.evaluate_state(state)[asp.get_start_state().player_to_move()], None]
 
         if depth == 0:
             return [self.heuristic(state), None]
